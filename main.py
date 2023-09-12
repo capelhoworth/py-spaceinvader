5d8df340aa7f283840dd5425620e671125322009
CapelHoworth <capelhoworth@gmail.com>
2023-09-12 15:52:06 -0400

    made purple window and changed icon

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import pygame
+
+# Initialize pygame
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# Title and Icon
+pygame.display.set_caption(" 🚀 Space Invaders 👾")
+icon = pygame.image.load('spaceship.png')
+pygame.display.set_icon(icon)
+
+# Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Screen Color RGB
+    screen.fill((128, 0, 128))
+    pygame.display.update()
