cc8c3951129498bded7b93fa8bdaa264a9a469e1
CapelHoworth <capelhoworth@gmail.com>
2023-09-19 14:53:13 -0400

    single enemy movement programmed

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ playerX_change = 0
 enemyImg = pygame.image.load('enemy.png')
 enemyX = random.randint(0, 800)
 enemyY = random.randint(50, 150)
-enemyX_change = 0
+enemyX_change = 0.8
+enemyY_change = 40
+
 
 
 def player(x, y): 
@@ -58,13 +60,24 @@ while running:
                 playerX_change = 0
 
 
+    # set player boundaries
     playerX += playerX_change
 
-    # set player boundaries
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
+
+
+    # set enemy boundaries
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 0.8
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.8
+        enemyY += enemyY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
