10c5106d029b9f5349224b923e233b0ef2cb9f7b
CapelHoworth <capelhoworth@gmail.com>
2023-09-20 09:08:35 -0400

    firing multiple times. ready to implement bullet collision logic

--- main.py
+++ main.py
@@ -76,7 +76,9 @@ while running:
             if event.key == pygame.K_RIGHT:
                 playerX_change = 1 
             if event.key == pygame.K_SPACE:
-                fire_bullet(playerX, bulletY)
+                if bullet_state == "ready":
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -103,8 +105,12 @@ while running:
         enemyY += enemyY_change
 
     # Bullet Movement
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+
     if bullet_state is "fire":
-        fire_bullet(playerX, bulletY)
+        fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
 
     player(playerX, playerY)
